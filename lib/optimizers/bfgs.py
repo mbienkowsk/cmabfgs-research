5a583429b8847c238a10d4c8c57409dd7314e770
Maksym <bienkowski.maksym@gmail.com>
2025-07-13 21:48:50 +0200

    feat: fill out bfgs implementation

--- lib/optimizers/bfgs.py
+++ lib/optimizers/bfgs.py
@@ -1,26 +1,55 @@
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import OptimizeResult, minimize
 
 if TYPE_CHECKING:
-    from lib.callbacks import ExperimentCallback
+    from lib.callbacks import BFGSMetricsCollector
 
 from lib.optimizers.base import Optimizer
 from lib.util import EvalCounter
 
 
+@dataclass
+class BFGSState:
+    counter: EvalCounter
+    current_result: OptimizeResult | None = field(default=None)
+
+    @property
+    def num_evaluations(self):
+        return self.counter.num_evaluations
+
+    @property
+    def best_solutions(self):
+        return self.counter.best_solutions
+
+
 class BFGS(Optimizer):
-    def __init__(self, x0: np.ndarray, seed: int, fun: EvalCounter):
+    state: BFGSState
+
+    def __init__(
+        self,
+        x0: np.ndarray,
+        seed: int,
+        fun: EvalCounter,
+        callback: "BFGSMetricsCollector",
+    ):
         self.x0 = x0
         self.inner = None
         self.seed = seed
-        self.fun = fun
+        self.state = BFGSState(fun)
+        self.callback = callback
 
-    def optimize(self, callback: "ExperimentCallback"):
+    def optimize(self):
+
+        def callback_wrapper(intermediate_result: OptimizeResult):
+            self.state.current_result = intermediate_result
+            return self.callback(self.state)
+
         minimize(
-            self.fun,
+            self.state.counter,
             self.x0,
             method="BFGS",
-            callback=callback,
+            callback=callback_wrapper,
         )
