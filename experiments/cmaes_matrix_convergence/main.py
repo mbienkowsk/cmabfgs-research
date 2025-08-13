70c3cfe4baa0d98aa60deb2f24e0fddd82eeb67e
Maksym <bienkowski.maksym@gmail.com>
2025-08-13 17:23:27 +0200

    feat: visual fixes for matrix convergence plotting

--- experiments/cmaes_matrix_convergence/main.py
+++ experiments/cmaes_matrix_convergence/main.py
@@ -64,30 +64,73 @@ def run_vanilla(x: np.ndarray, seed: int, idx: int):
     logger.info(f"{idx}: done")
 
 
-def visualize_results(result_path: Path):
-    data = load_results_from_directory(result_path)
-    dim = extract_dim_from_path(result_path)
-    objective = extract_objective_from_path(result_path)
+def visualize_results(read_from: Path, save_to: Path):
+    data = load_results_from_directory(read_from)
+    dim = extract_dim_from_path(read_from)
+    objective = extract_objective_from_path(read_from)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4), sharex=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 6), sharex=True)
 
-    ax1.plot(data.index, data.iloc[:, 1], label=data.columns[1])
+    ax1.plot(data.index, data["cov_cond"])
     ax1.set_title("Współczynnik uwarunkowania C vs liczba ewaluacji")
     ax1.set_ylabel("cond(C)")
     ax1.legend()
 
-    ax2.plot(data.index, data.iloc[:, 2], label=data.columns[2])
+    ax2.plot(data.index, data["cov_diff_sq"])
     ax2.set_title("Kwadrat zmiany C vs liczba ewaluacji")
+    ax2.set_ylabel("cov_diff_sq")
 
-    for ax in (ax1, ax2):
+    ax3.plot(data.index, data["best"])
+    ax3.set_title("Wartość f. straty w najlepszym punkcie vs liczba ewaluacji")
+    ax3.set_ylabel("loss(x_best)")
+
+    for ax in (ax1, ax2, ax3):
         ax.set_yscale("log")
         ax.set_xscale("log")
         ax.set_xlabel("liczba ewaluacji")
         ax.legend()
+        ax.grid(which="both")
 
     fig.suptitle(f"Funkcja {objective} w {dim} wymiarach")
     fig.tight_layout()
-    fig.savefig(result_path / f"{objective}_{dim}.png", dpi=300)
+    fig.savefig(save_to / f"{objective}_{dim}.png", dpi=300)
+
+
+def visualize_aggregated_results(read_from: Path, save_to: Path):
+    """visualize results on a single plot to compare trends"""
+    data = load_results_from_directory(read_from)
+    dim = extract_dim_from_path(read_from)
+    objective = extract_objective_from_path(read_from)
+
+    fig, ax1 = plt.subplots(figsize=(9, 6))
+
+    ax1.plot(data.index, data["cov_cond"], label="cov_cond", color="C0")
+    ax1.set_ylabel("cov_cond", color="C0")
+    ax1.set_xscale("log")
+    ax1.set_yscale("log")
+    ax1.tick_params(axis="y", labelcolor="C0")
+
+    ax2 = ax1.twinx()
+    ax2.plot(data.index, data["cov_diff_sq"], label="cov_diff_sq", color="C1")
+    ax2.set_ylabel("cov_diff_sq", color="C1")
+    ax2.set_yscale("log")
+    ax2.tick_params(axis="y", labelcolor="C1")
+
+    ax3 = ax1.twinx()
+    ax3.spines["right"].set_position(("outward", 60))
+    ax3.plot(data.index, data["best"], label="best", color="C2")
+    ax3.set_ylabel("loss(x_best)", color="C2")
+    ax3.set_yscale("log")
+    ax3.tick_params(axis="y", labelcolor="C2")
+
+    ax1.set_xlabel("liczba ewaluacji f.celu")
+    fig.suptitle(
+        f"Relacja między wartością najlepszego punktu, wsp. uwarunkowania C i \nkwadratem zmiany C. Funkcja {objective}, {dim} wymiarów"
+    )
+    ax1.grid(which="both")
+    fig.tight_layout()
+    fig.legend()
+    fig.savefig(save_to / f"{objective}_{dim}_agg.png", dpi=300)
 
 
 def single_run(idx: int):
@@ -115,6 +158,8 @@ if __name__ == "__main__":
         shutil.rmtree(RESULT_DIR)
 
     os.makedirs(RESULT_DIR)
+    os.makedirs(PLOT_EXPORT_DIR, exist_ok=True)
 
     main()
-    visualize_results(PLOT_EXPORT_DIR)
+    visualize_results(RESULT_DIR, PLOT_EXPORT_DIR)
+    visualize_aggregated_results(RESULT_DIR, PLOT_EXPORT_DIR)
