1d99c29bf57b715723f602a4412d2af5106b3b5a
Maksym <bienkowski.maksym@gmail.com>
2025-09-12 13:28:22 +0200

    feat: multicmafbgs, grid search for the best switch spot

---
+++ experiments/switch_grid_search/switch_grid_search.py
@@ -0,0 +1,142 @@
+import multiprocessing as mp
+import os
+import sys
+from pathlib import Path
+from typing import Callable, cast
+
+import matplotlib.pyplot as plt
+import numpy as np
+from loguru import logger
+from pandas import DataFrame
+from sympy import prime
+
+from lib.callbacks import MetricsCollector
+from lib.funs import get_function_by_name
+from lib.metrics import BestSoFar
+from lib.optimizers.bfgs import BFGS
+from lib.optimizers.multicmabfgs import MultiCMABFGS
+from lib.serde import aggregate_dataframes
+from lib.stopping import CMAESEarlyStopping
+from lib.util import EvalCounter
+
+BOUNDS = 100
+DIMENSIONS = int(os.environ["DIMENSIONS"])
+NUM_RUNS = int(os.environ["N_RUNS"])
+OBJECTIVE_NAME = os.environ["OBJECTIVE"]
+SWITCH_AFTER_ITERATIONS = list(map(int, os.environ["SWITCH_AFTER"].split("-")))
+
+OBJECTIVE, OPTIMUM = cast(
+    tuple[Callable, float],
+    get_function_by_name(OBJECTIVE_NAME, DIMENSIONS, with_optimum=True),
+)
+MAXEVALS = 4000 * DIMENSIONS
+POPULATION_SIZE = 4 * DIMENSIONS
+
+
+RESULT_DIR = Path(__file__).parent / "results"
+PLOT_EXPORT_DIR = RESULT_DIR / "plots"
+
+
+def run_multicmabfgs(x: np.ndarray, seed: int, idx: int):
+    counter = EvalCounter(OBJECTIVE)
+    metrics = [
+        BestSoFar(OPTIMUM),
+    ]
+    callback = MetricsCollector(metrics, "cmabfgs")
+    optimizer = MultiCMABFGS(
+        x,
+        SWITCH_AFTER_ITERATIONS,
+        seed,
+        counter,
+        POPULATION_SIZE,
+        callback,
+        CMAESEarlyStopping(MAXEVALS, tolfun=1e-9),
+    )
+    optimizer.optimize()
+    return callback.as_dataframe()
+
+
+def run_bfgs(x: np.ndarray, seed: int, idx: int):
+    counter = EvalCounter(OBJECTIVE)
+    metrics = [BestSoFar(OPTIMUM)]
+    callback = MetricsCollector(metrics, "bfgs")
+    bfgs = BFGS(x, seed=seed, fun=counter, callback=callback)
+    bfgs.optimize()
+    logger.info(f"{idx}: done with BFGS")
+    return callback.as_dataframe()
+
+
+def single_run(idx: int) -> tuple[DataFrame, DataFrame]:
+    seed: int = prime(idx)  # pyright: ignore[reportAssignmentType]
+    rng = np.random.default_rng(seed)
+    x = cast(
+        np.ndarray,  # pyright: ignore[reportArgumentType]
+        (rng.random(DIMENSIONS) - 0.5)
+        * 2
+        * BOUNDS,  # pyright: ignore[reportArgumentType]
+    )
+    cmabfgs = run_multicmabfgs(x, seed, idx)
+    bfgs = run_bfgs(x, seed, idx)
+    return cmabfgs, bfgs
+
+
+def visualize_results(bfgs: DataFrame, cmabfgs: DataFrame, save_to: Path):
+    plt.figure(figsize=(9, 6))
+    plt.plot(bfgs.index, bfgs["best"], label="BFGS")
+    for i, val in enumerate(SWITCH_AFTER_ITERATIONS):
+        plt.plot(
+            cmabfgs.index,
+            cmabfgs[f"best_{val}"],
+            label=f"CMABFGS (switch po {val} iteracjach/{val*POPULATION_SIZE} ewaluacjach)",
+        )
+
+        ymin, ymax = plt.ylim()
+        plt.vlines(
+            x=np.array(SWITCH_AFTER_ITERATIONS) * POPULATION_SIZE,
+            ymin=ymin,
+            ymax=ymax,
+            colors="r",
+            linestyles="dashed",
+        )
+
+    plt.plot(
+        cmabfgs.index,
+        cmabfgs[f"best_vanilla_cmaes"],
+        label=f"CMA-ES",
+    )
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.grid(which="both")
+    plt.xlabel("Liczba ewaluacji")
+    plt.legend()
+    plt.ylabel("best so far")
+    plt.title(
+        f"BFGS vs CMAES vs CMABFGS, funkcja {OBJECTIVE_NAME}, {DIMENSIONS} wymiarów"
+    )
+    plt.savefig(save_to / f"{OBJECTIVE_NAME}_{DIMENSIONS}.png", dpi=300)
+
+
+def main():
+    with mp.Pool(mp.cpu_count()) as pool:
+        rv = pool.map(single_run, range(1, NUM_RUNS + 1))
+
+    cmabfgs_agg = aggregate_dataframes([val[0] for val in rv])
+    bfgs_agg = aggregate_dataframes([val[1] for val in rv])
+
+    # Prefix columns
+    bfgs_prefixed = bfgs_agg.add_prefix("bfgs_")
+    # Concatenate along columns
+    combined = bfgs_prefixed.join(cmabfgs_agg, how="outer")
+    # Save to CSV
+    combined.to_csv(RESULT_DIR / f"{OBJECTIVE_NAME}_{DIMENSIONS}_combined.csv")
+    visualize_results(bfgs_agg, cmabfgs_agg, save_to=PLOT_EXPORT_DIR)
+
+
+if __name__ == "__main__":
+    logger.remove()
+    logger.add(sys.stderr, level="ERROR")
+
+    os.makedirs(RESULT_DIR, exist_ok=True)
+    os.makedirs(PLOT_EXPORT_DIR, exist_ok=True)
+
+    main()
--- lib/callbacks.py
+++ lib/callbacks.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Protocol, Sequence
+from typing import Protocol, Sequence, cast
 
 import pandas as pd
 
@@ -20,20 +20,26 @@ class MetricsCollector:
 
     def __call__(self, state: HasCounter):
         evals = state.counter.num_evaluations
+
         entry = {"num_evaluations": [evals]}
         for metric in self.metrics:
             collect_fn = getattr(metric, f"collect_{self.collect_method}")
-            entry[metric.key()] = [collect_fn(state)]
+            key = (
+                metric.key() + "_" + suffix
+                if (suffix := getattr(state, "key_suffix", ""))
+                else metric.key()
+            )
+            entry[key] = [collect_fn(state)]
 
-        entry_df = pd.DataFrame(entry).dropna()
+        entry_df = pd.DataFrame(entry).set_index("num_evaluations")
         if self.data.empty:
             self.data = entry_df
         else:
             self.data = pd.concat([self.data, entry_df])
 
     def as_dataframe(self):
-        self.data.set_index("num_evaluations", inplace=True)
-        return self.data
+        # squash entries with duplicate indices
+        return cast(pd.DataFrame, self.data.groupby(self.data.index).max())
 
     def export_to_csv(self, path: Path):
         self.as_dataframe().to_csv(path)
