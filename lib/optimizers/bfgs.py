108821968085899ecc4d0f3cb24898d544a90605
Maksym <bienkowski.maksym@gmail.com>
2025-10-26 13:42:05 +0100

    fix: make sure bfgs evaluates at least once, even for flat functions

--- lib/optimizers/bfgs.py
+++ lib/optimizers/bfgs.py
@@ -43,6 +43,10 @@ class BFGS(Optimizer):
         self.callback = callback
 
     def optimize(self):
+        self.state.counter(
+            self.x0
+        )  # ensures there is a single evaluation even if bfgs quits instantly
+        self.callback(self.state)
 
         def callback_wrapper(intermediate_result: OptimizeResult):
             self.state.current_result = intermediate_result
