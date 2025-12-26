198d5aa37f88257115b9d6c87fd980f0284f0b9e
Maksym <bienkowski.maksym@gmail.com>
2025-12-26 17:16:10 +0100

    chore: update paths in visualization of quad conv

--- experiments/quad_convergence/visualize.py
+++ experiments/quad_convergence/visualize.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 DIM = 10
-
-RESULT_PATH = Path(__file__).parent / "results" / f"d{DIM}" / f"bfgs_d{DIM}.parquet"
+RESULT_PATH = Path(__file__).parent / "results" / f"d{DIM}" / "agg"
 
 
 def visualize_results(df: pd.DataFrame):
-    df[df.index < 400].plot(
+    df.plot(
         title="BFGS inicjowany macierzą kowariancji CMAESa po n iteracjach - porównanie krzywych zbieżności"
     )
     plt.yscale("log")
@@ -18,4 +17,5 @@ def visualize_results(df: pd.DataFrame):
 
 
 if __name__ == "__main__":
-    visualize_results(pd.read_parquet(RESULT_PATH))
+    visualize_results(pd.read_parquet(RESULT_PATH / "bfgs.parquet"))
+    visualize_results(pd.read_parquet(RESULT_PATH / "bfgs_inherited_x0.parquet"))
