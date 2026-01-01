db7cf24eb1abd34f3a256a9eb6def6bf440e9f96
Maksym <bienkowski.maksym@gmail.com>
2026-01-01 14:09:50 +0100

    refactor: factor out experimentconfigbase

--- experiments/find_switch_interval/cmaes/experiment_config.py
+++ experiments/find_switch_interval/cmaes/experiment_config.py
@@ -1,55 +1,26 @@
-import os
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import override
 
-from loguru import logger
-
-from experiments.find_switch_interval.common import ObjectiveChoice, OptimumPosition
-from lib.funs import get_function_by_name
+from experiments.find_switch_interval.common import (
+    ExperimentConfigBase,
+)
 
 
 @dataclass
-class CMAESExperimentConfig:
-    dimensions: int
-    num_runs: int
-    objective_choice: ObjectiveChoice
-    optimum_position: OptimumPosition
-    debug: bool = False
-
+class CMAESExperimentConfig(ExperimentConfigBase):
     max_evals: int = field(init=False)
     popsize: int = field(init=False)
     collection_interval: int = field(init=False)
-    bounds: tuple[float, float] = field(init=False)
 
     def __post_init__(self):
+        super().__post_init__()
         self.max_evals = 10_000 * self.dimensions
         self.popsize = 4 * self.dimensions
         self.collection_interval = self.dimensions // 2
-        self.bounds = self.optimum_position.get_bounds()
-        self.output_directory.mkdir(parents=True, exist_ok=True)
-
-    @classmethod
-    def create_from_env(cls):
-        try:
-            return cls(
-                dimensions=int(os.environ["DIMENSIONS"]),
-                num_runs=int(os.environ["N_RUNS"]),
-                objective_choice=ObjectiveChoice(os.environ["OBJECTIVE_CHOICE"]),
-                optimum_position=OptimumPosition(
-                    os.environ["OPTIMUM_POSITION"],
-                ),
-            )
-        except KeyError as e:
-            logger.error(f"Environment variable {e} is not set.")
-            raise e
-
-    def get_objective_instance(self):
-        return get_function_by_name(self.objective_choice.value, self.dimensions)
-
-    def get_run_indices(self):
-        return range(1, self.num_runs)
 
     @property
+    @override
     def output_directory(self):
         return (
             Path(__file__).parent
--- experiments/find_switch_interval/common.py
+++ experiments/find_switch_interval/common.py
@@ -1,4 +1,10 @@
+import os
+from dataclasses import dataclass, field
 from enum import Enum
+
+from loguru import logger
+
+from lib.funs import get_function_by_name
 
 
 class ObjectiveChoice(Enum):
@@ -30,3 +36,42 @@ class OptimumPosition(Enum):
             case OptimumPosition.OUTSIDE_CORNER:
                 label = "poza rogiem obszaru dopuszczalnego"
         return f"{label} (granice: {bounds})"
+
+
+@dataclass
+class ExperimentConfigBase:
+    dimensions: int
+    num_runs: int
+    objective_choice: ObjectiveChoice
+    optimum_position: OptimumPosition
+    debug: bool = False
+    bounds: tuple[float, float] = field(init=False)
+
+    def __post_init__(self):
+        self.bounds = self.optimum_position.get_bounds()
+        self.output_directory.mkdir(parents=True, exist_ok=True)
+
+    @property
+    def output_directory(self):
+        raise NotImplementedError()
+
+    def get_run_indices(self):
+        return range(1, self.num_runs)
+
+    def get_objective_instance(self):
+        return get_function_by_name(self.objective_choice.value, self.dimensions)
+
+    @classmethod
+    def create_from_env(cls):
+        try:
+            return cls(
+                dimensions=int(os.environ["DIMENSIONS"]),
+                num_runs=int(os.environ["N_RUNS"]),
+                objective_choice=ObjectiveChoice(os.environ["OBJECTIVE_CHOICE"]),
+                optimum_position=OptimumPosition(
+                    os.environ["OPTIMUM_POSITION"],
+                ),
+            )
+        except KeyError as e:
+            logger.error(f"Environment variable {e} is not set.")
+            raise e
