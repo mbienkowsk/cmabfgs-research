0f7ac276ff7c70ec2c4f70ad7e137dbcdfd90f29
Maksym <bienkowski.maksym@gmail.com>
2026-01-11 23:00:24 +0100

    chore: further work on fsi visualization

--- experiments/find_switch_interval/cmabfgs/visualize.py
+++ experiments/find_switch_interval/cmabfgs/visualize.py
@@ -1,14 +1,18 @@
+import os
 from dataclasses import dataclass
+from itertools import product
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from joblib import Parallel, delayed
 
 from experiments.find_switch_interval.cmabfgs.experiment_config import (
     CMABFGSExperimentConfig,
 )
 from experiments.find_switch_interval.common import ObjectiveChoice, OptimumPosition
 from lib.enums import HessianNormalization
+from lib.plotting_util import configure_mpl_for_manuscript
 
 ANY_INT = 0
 
@@ -50,11 +54,11 @@ class CMABFGSPlotter:
 
     def get_label_from_mul(self, mul: float) -> str:
         if mul == 0:
-            return "Vanilla BFGS"
-        iters = int(self.config.dimensions * mul)
-        return f"Przełączenie co {iters} iteracji"
+            return "BFGS"
+        return f"CMABFGS, $k_{{{'mul'}}}={mul}$"
 
     def plot(self, agg_df: pd.DataFrame, cmaes_df: pd.DataFrame):
+        configure_mpl_for_manuscript()
         fig, ax = plt.subplots(figsize=(16, 11))
 
         secax = ax.secondary_xaxis(
@@ -64,7 +68,7 @@ class CMABFGSPlotter:
                 lambda x: x * 4 * self.config.dimensions,  # pyright: ignore[reportOperatorIssue]
             ),
         )
-        secax.spines["bottom"].set_position(("outward", 40))
+        secax.spines["bottom"].set_position(("outward", 80))
 
         for mul, mul_df in agg_df.groupby("multiplier"):
             mul_df.plot(
@@ -75,13 +79,13 @@ class CMABFGSPlotter:
                 label=self.get_label_from_mul(mul),  # pyright: ignore[reportArgumentType]
             )
 
-        cmaes_df.plot(ax=ax, logy=True, y="best_cmaes", label="vanilla CMA-ES")
+        cmaes_df.plot(ax=ax, logy=True, y="best_cmaes", label="CMA-ES")
         ax.grid()
+        title = f"d={self.config.dimensions}, f={self.config.objective_choice.value}"
+        if not self.config.objective_choice.value.startswith("CEC"):
+            title += f", optimum {self.config.optimum_position.to_plot_label()}"
 
-        plt.title(
-            f"Krzywe zbieżności CMA-ES i CMABFGS (d={self.config.dimensions}, f={self.config.objective_choice.value}, optimum {self.config.optimum_position.to_plot_label()})\n\
-            Hesjan: {self.config.hess_normalization.to_plot_label()}"
-        )
+        plt.title(title)
         secax.set_xlabel("Iteracje CMA-ES")
         ax.set_xlabel("Liczba ewaluacji funkcji celu")
         plt.tight_layout()
@@ -90,6 +94,7 @@ class CMABFGSPlotter:
         else:
             self.plot_save_path.parent.mkdir(parents=True, exist_ok=True)
             plt.savefig(self.plot_save_path, dpi=300)
+        plt.close()
 
     def run(self):
         agg_df = self.load_agg_df()
@@ -97,13 +102,57 @@ class CMABFGSPlotter:
         self.plot(agg_df, cmaes_df)
 
 
+def plot_config(config: CMABFGSExperimentConfig):
+    plotter = CMABFGSPlotter(config, save_to_disk=True)
+    plotter.run()
+
+
 if __name__ == "__main__":
-    config = CMABFGSExperimentConfig(
-        100,
-        ANY_INT,
-        ObjectiveChoice.CEC1,
-        OptimumPosition.MIDDLE,
-        True,
-        HessianNormalization.UNIT,
-    )
-    CMABFGSPlotter(config, save_to_disk=False).run()
+    debug = bool(os.getenv("DEBUG", ""))
+    print(f"Debug mode: {debug}")
+
+    if debug:
+        config = CMABFGSExperimentConfig(
+            100,
+            ANY_INT,
+            ObjectiveChoice.ELLIPTIC,
+            OptimumPosition.MIDDLE,
+            True,
+            HessianNormalization.UNIT,
+        )
+        CMABFGSPlotter(config, save_to_disk=False).run()
+
+    else:
+        optimum_positions = [
+            OptimumPosition.MIDDLE,
+        ]
+        hess_norms = [
+            HessianNormalization.UNIT,
+        ]
+
+        # cec dims vary from control dims
+        cec_dims = [10, 30, 50, 100]
+        cec_objectives = [getattr(ObjectiveChoice, f"CEC{i}") for i in range(1, 31)]
+        cec_configurations = [
+            CMABFGSExperimentConfig(d, ANY_INT, obj, opt, False, hess_norm)
+            for d, obj, opt, hess_norm in product(
+                cec_dims, cec_objectives, optimum_positions, hess_norms
+            )
+        ]
+
+        control_dims = [10, 20, 50, 100]
+        control_objectives = [
+            ObjectiveChoice.ELLIPTIC,
+            ObjectiveChoice.RASTRIGIN,
+        ]
+        control_configurations = [
+            CMABFGSExperimentConfig(d, ANY_INT, obj, opt, False, hess_norm)
+            for d, obj, opt, hess_norm in product(
+                control_dims, control_objectives, optimum_positions, hess_norms
+            )
+        ]
+        all_configurations = cec_configurations + control_configurations
+
+        Parallel(n_jobs=-1, backend="loky")(
+            delayed(plot_config)(config) for config in all_configurations
+        )
